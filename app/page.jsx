3c66557de243f665cf8180caf9b9291cef2f859a
ambersr <amber.schalker@hva.nl>
2025-10-27 15:11:09 +0100

    feat: installation react + test pagina #1

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import Button from '@/components/Button'
+
+export default function Page() {
+  return (
+    <main>
+        <h1>Hello, Next.js!</h1>
+        <Button/>
+    </main>
+  )
+}
--- src/app/about/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function AboutPage() {
-  return (
-    <main>
-      <h1>Over mij</h1>
-      <p>Hier vertel ik iets over mezelf.</p>
-    </main>
-  );
-}

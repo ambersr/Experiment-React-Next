1f47a8ef0163cc96f9ed650933c979645ab4676c
ambersr <amber.schalker@hva.nl>
2025-10-31 14:29:08 +0100

    feat: data documents ophalen home #2

---
+++ src/app/page.jsx
@@ -0,0 +1,29 @@
+import DocumentCard from "@/components/document/DocumentCard";
+import styles from "./page.module.css";
+
+export default async function Page() {
+    const baseUrl = "https://fdnd-agency.directus.app/items/";
+    const documentsEndpoint = "adconnect_documents";
+    const fields = "?fields=title,id,description,slug,hero_image,source_file,category.*,date";
+
+    const documentUrl = baseUrl + documentsEndpoint + fields;
+
+    const documentsResponse = await fetch(documentUrl);
+    const documentsData = await documentsResponse.json();
+
+    const documents = documentsData.data;
+
+    return (
+
+         <main className={styles.main}>
+            <h1 style={{textAlign: "center"}}>Overzicht documenten</h1>
+
+            <ul>
+                {documents.map((doc) => (
+                    <DocumentCard key={doc.id} document={doc} />
+                ))}
+            </ul>
+        </main>
+
+    )
+}

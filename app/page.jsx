e2f693a4529a0cf8031930235b6617946d9de9c0
ambersr <amber.schalker@hva.nl>
2025-10-31 10:32:19 +0100

    feat: data ophalen documents #2

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,30 @@
-import Button from '@/components/Button'
+import Button from '@/components/button/Button'
 
-export default function Page() {
-  return (
-    <main>
-        <h1>Hello, Next.js!</h1>
-        <Button/>
-    </main>
-  )
+import DocumentCard from "@/components/document/DocumentCard";
+
+export default async function Page() {
+    const baseUrl = "https://fdnd-agency.directus.app/items/";
+    const documentsEndpoint = "adconnect_documents";
+    const fields = "?fields=title,id,description,slug,hero_image,source_file,category.*,date";
+
+    const documentUrl = baseUrl + documentsEndpoint + fields;
+
+    const documentsResponse = await fetch(documentUrl);
+    const documentsData = await documentsResponse.json();
+
+    const documents = documentsData.data;
+
+    return (
+        <main>
+
+            <h1>Hello, Next.js!</h1>
+            <Button/>
+
+            <ul>
+                {documents.map((doc) => (
+        <DocumentCard key={doc.id} document={doc} />
+      ))}
+            </ul>
+        </main>
+    )
 }
